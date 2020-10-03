fabd1af9f83938c4e3e568abc384f609d38afcaa
galvezn <72047040+galvezn@users.noreply.github.com>
2020-10-02 19:28:24 -0500

    Completed Lab

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  puts "Polly wants a cracker!"
+end
+
+# when run will output the string whenever using this method.
