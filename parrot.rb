306584f3461c54eb3ca4e67ee29caf1f909acea4
galvezn <72047040+galvezn@users.noreply.github.com>
2020-10-02 19:52:58 -0500

    Update parrot.rb
    done with lab

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot
-  puts "Polly wants a cracker!"
+   puts phrase = "squawk!"
 end
 
 # when run will output the string whenever using this method.
+
+parrot
